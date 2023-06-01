d4f80735768b3c4e66878f58f36e72a9fae8369b
jo.gonzalez <jo.gonzalez@ufl.edu>
2023-06-01 13:27:05 -0400

    ...

--- leaflet-dynmap.js
+++ leaflet-dynmap.js
@@ -170,6 +170,7 @@ let loadVarData = function loadVarData(fileName, idxDate, varName, dims) {
 
     console.log(">>>>>", varName)
     const fullFileName = fileName + '?' + varName + '%5B' + (idxDate + 1) + '%5D%5B0:1:' + (dims[1] -1) + '%5D%5B0:1:' + (dims[0] -1) + '%5D';
+    console.log(">>>>>", fullFileName)
     [dimsData, data] = window.loadBinaryDODSFloat32Cached(fullFileName);
 
 

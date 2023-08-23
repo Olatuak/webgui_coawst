7f1be0235a2414966d20d0258cff62339ca8f811
jo.gonzalez <jo.gonzalez@ufl.edu>
2023-08-23 09:47:58 -0400

    ...

--- maps.py
+++ maps.py
@@ -182,7 +182,7 @@ class Maps:
                         gridType = layer['gridtype'].split(',')
                         if len(gridType) == 1:
                             print(6666)
-                            aio.sleep(1111)
+                            sleep(11)
                             print(6667)
                             dynLayer, times = window.addNewDynHeatmapLayer(mapLayer, fileName,
                                                             layer['name'], layer['server']['grids'][gridType[0]],

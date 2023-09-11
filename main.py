90d1edc409789ed86572d0a24ee81be2aadd4d0e
jo.gonzalez <jo.gonzalez@ufl.edu>
2023-09-11 15:59:57 -0400

    ...

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def onDateChange(layer, date):
 def onFileDateChange(event):
     # the date of the data file changes (do not confuse with onFileChange)
     print('XXXXXXXXX')
-    print(event.target.__dir__)
+    print(datetime.strptime(event.target.value, '%Y-%m-%d')
     mapLayers.clearAll()
 
     pass
